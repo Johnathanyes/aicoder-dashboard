3e7ed3764693a239121e46fcb0d34df5a59ca89b
Johnathanyes <johnathanylee327@gmail.com>
2025-11-16 17:56:37 -0500

    updated serverexternalpackages to fix turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ['@libsql/client', 'libsql',"@payloadcms/db-sqlite"]
 };
 
 export default nextConfig;
